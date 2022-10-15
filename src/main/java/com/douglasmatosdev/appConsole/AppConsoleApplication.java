d981dc9101f1def8432831a5b608a7eac79f484d
douglasmatosdev <douglasmatosdev@gmail.com>
2022-10-15 15:06:54 -0300

    variables

--- src/main/java/com/douglasmatosdev/appConsole/AppConsoleApplication.java
+++ src/main/java/com/douglasmatosdev/appConsole/AppConsoleApplication.java
@@ -1,16 +1,38 @@
 package com.douglasmatosdev.appConsole;
 
+import java.io.*;
+
 // import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppConsoleApplication {
 
-	public static void main(String[] args) {
-		System.out.println("=====================");
-		System.out.println("Welcome to app console in Spring Boot");
-		System.out.println("=====================");
+	/*
+	 * João é um comerciante que vende laranjas.
+	 * Ele precisa fazer uma entrega de algumas coixas.
+	 * Cada laranja ele vende por 0,50 centavosm cada caixa tem 50 laranjas.
+	 * 
+	 * Faça um programa que peça a quantidade de caixas para o joão e de o total de
+	 * receber.
+	 */
 
+	public static void main(String[] args) {
+		System.out.println("============================");
+		System.out.println("		Welcome!!!			");
+		System.out.println("============================");
+
+		Console cnsl = System.console();
+		int quantidadeDeCaixas = Integer.parseInt(cnsl.readLine("Digite a quantidade de caixas: \n"));
+
+		double valorDaLaranjaUnidade = 0.50;
+		int quantidadeDeLaranjaPorCaixa = 50;
+		int totalDeLaranjas = quantidadeDeCaixas * quantidadeDeLaranjaPorCaixa;
+		double valorTotal = totalDeLaranjas * valorDaLaranjaUnidade;
+
+		System.out.println("============================================");
+		System.out.println("Valor total a receber é de R$ " + valorTotal);
+		System.out.println("============================================");
 
 		// SpringApplication.run(AppConsoleApplication.class, args);
 	}
