ce79fe3c0ec4ee4655072ce74961a3d8d3962843
douglasmatosdev <douglasmatosdev@gmail.com>
2022-10-15 14:53:42 -0300

    first commit

---
+++ src/main/java/com/douglasmatosdev/appConsole/AppConsoleApplication.java
@@ -0,0 +1,18 @@
+package com.douglasmatosdev.appConsole;
+
+// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConsoleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("=====================");
+		System.out.println("Welcome to app console in Spring Boot");
+		System.out.println("=====================");
+
+
+		// SpringApplication.run(AppConsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglasmatosdev/appConsole/AppConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasmatosdev.appConsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
